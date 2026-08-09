9fd727a61e223e08fc66d062be186d646409cfcb
GennadiiIurkevych <yourgensport@gmail.com>
2026-08-09 16:16:57 +0300

    fixed cqrs-pattern

--- src/EcomCourse.Application/Abstraction/Messaging/ICommand.cs
+++ src/EcomCourse.Application/Abstraction/Messaging/ICommand.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace EcomCourse.Application.Abstraction.Messaging
 {
