e949252ab3bcd960ce004a3c5839fc60e60e9d42
Shkkilet <mguk704@gmail.com>
2026-08-26 16:35:25 +0300

    commit before merge

---
+++ src/EcomCourse.Application/Authentication/DTOs/ApplicationUserDto.cs
@@ -0,0 +1,6 @@
+namespace EcomCourse.Application.Authentication.DTOs
+{
+    public record ApplicationUserDto(
+        Guid Id,
+        string Email);
+}
--- src/EcomCourse.Application/Services/CompensateAsync.cs
+++ src/EcomCourse.Application/Services/CompensateAsync.cs
@@ -1,34 +1,34 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
+//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace EcomCourse.Application.Services
-{
-    internal class CompensateAsync
-    {
-        private async Task CompensateAsync(
-    ApplicationUser user,
-    Guid customerId,
-    CancellationToken cancellationToken)
-        {
-            if (customerId != Guid.Empty)
-            {
-                var customer =
-                    await _context.Customers
-                        .FirstOrDefaultAsync(
-                            x => x.Id == customerId,
-                            cancellationToken);
+//namespace EcomCourse.Application.Services
+//{
+//    internal class CompensateAsync
+//    {
+//        private async Task CompensateAsync(
+//    Applica user,
+//    Guid customerId,
+//    CancellationToken cancellationToken)
+//        {
+//            if (customerId != Guid.Empty)
+//            {
+//                var customer =
+//                    await _context.Customers
+//                        .FirstOrDefaultAsync(
+//                            x => x.Id == customerId,
+//                            cancellationToken);
 
-                if (customer is not null)
-                {
-                    _context.Customers.Remove(customer);
+//                if (customer is not null)
+//                {
+//                    _context.Customers.Remove(customer);
 
-                    await _context.SaveChangesAsync(
-                        cancellationToken);
-                }
-            }
+//                    await _context.SaveChangesAsync(
+//                        cancellationToken);
+//                }
+//            }
 
-            await _manager.DeleteAsync(user);
-        }
-    }
-}
+//            await _manager.DeleteAsync(user);
+//        }
+//    }
+//}
