c743647042c31f4071a285f1d42bd7fdf9721431
Norejon <oreonorej@gmail.com>
2026-08-28 15:18:45 +0300

    refactor: remove mediatr behavior leftovers from exception handler branch

--- src/EcomCourse.Application/DependencyInjection.cs
+++ src/EcomCourse.Application/DependencyInjection.cs
@@ -1,5 +1,3 @@
-using EcomCourse.Application.Behaviors;
-using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace EcomCourse.Application;
@@ -8,18 +6,10 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        var assembly = typeof(DependencyInjection).Assembly;
-
         services.AddMediatR(cfg =>
         {
-            cfg.RegisterServicesFromAssembly(assembly);
-
-            cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
-            cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
-            cfg.AddOpenBehavior(typeof(PerformanceBehavior<,>));
+            cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
         });
-
-        services.AddValidatorsFromAssembly(assembly, includeInternalTypes: true);
 
         return services;
     }
