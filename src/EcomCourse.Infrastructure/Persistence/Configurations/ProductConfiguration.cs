241150fa7736c3ddf8a33728ee8a9da52f3cbfac
GennadiiIurkevych <yourgensport@gmail.com>
2026-09-06 18:43:48 +0300

    Add Product persistence configuration

---
+++ src/EcomCourse.Infrastructure/Persistence/Configurations/ProductConfiguration.cs
@@ -0,0 +1,52 @@
+using EcomCourse.Domain.Categories;
+using EcomCourse.Domain.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EcomCourse.Infrastructure.Persistence.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Products");
+
+            builder.HasKey(product => product.Id);
+
+            builder.Property(product => product.Name)
+            .HasMaxLength(100)
+            .IsRequired();
+
+            builder.OwnsOne(product => product.Price, priceBuilder =>
+            {
+                priceBuilder.Property(price => price.Amount)
+                    .HasColumnName("PriceAmount")
+                    .HasPrecision(18, 2)
+                    .IsRequired();
+
+                priceBuilder.Property(price => price.Currency)
+                    .HasConversion<string>()
+                    .HasColumnName("Currency")
+                    .IsRequired();
+            });
+
+            builder.OwnsOne(product => product.SKU, skuBuilder =>
+            {
+                skuBuilder.Property(sku => sku.Value)
+                    .HasColumnName("SKUValue")
+                    .HasMaxLength(8)
+                    .IsRequired();
+
+                skuBuilder.HasIndex(sku => sku.Value)
+                    .IsUnique();
+            });
+
+            builder.Property(product => product.CategoryId)
+                .IsRequired();
+
+            builder.HasOne<Category>()
+                .WithMany()
+                .HasForeignKey(product => product.CategoryId);
+        }
+    }
+}
